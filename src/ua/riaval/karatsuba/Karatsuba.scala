314c7290d1c2bede9c269fd20efe4a38bdcd752d
riaval <ls2294@gmail.com>
2015-02-11 09:25:56 +0200

    it's working

---
+++ src/ua/riaval/karatsuba/Karatsuba.scala
@@ -0,0 +1,26 @@
+package ua.riaval.karatsuba
+
+object Karatsuba {
+
+  val T = 10
+  val MIN_LENGTH = 4
+
+  /*
+  * Simple notation
+  * acT^2 + ((a + b)(c + d) - ac - bd)T + bd
+  * */
+  def multiplication(first: BigInt, second: BigInt): BigInt =
+    if (first.bitLength > MIN_LENGTH || second.bitLength > MIN_LENGTH) first * second
+    else {
+      val (a, b) = split(first)
+      val (c, d) = split(second)
+
+      multiplication(a , c) * T * T + (multiplication(a + b , c + d) - multiplication(a , c) - multiplication(b , d)) * T + multiplication(b , d)
+    }
+
+  private def split(number: BigInt): (BigInt, BigInt) = {
+    if (number.toString().length == 1) (0, number)
+    else number /% 10
+  }
+
+}
--- src/ua/riaval/karatsuba/Main.scala
+++ src/ua/riaval/karatsuba/Main.scala
@@ -2,34 +2,8 @@ package ua.riaval.karatsuba
 
 object Main {
 
-  val T = 10
-  val MIN_LENGTH = 4
-
   def main(args: Array[String]) {
-    println(BigInt(35).bitCount)
-
-//    println(
-//      karatsuba(12, 23)
-//    )
+    println(Karatsuba.multiplication(124456, 235346))
   }
-
-  def split(number: BigInt): (BigInt, BigInt) = {
-    if (number.bitLength < 2) null
-    else number /% 10 //(number / 10, number % 10)
-  }
-
-  /*
-  * Simple notation
-  * acT^2 + ((a + b)(c + d) - ac - bd)T + bd
-  * */
-  def karatsuba(first: BigInt, second: BigInt): BigInt =
-    if (first.bitLength > MIN_LENGTH || second.bitLength > MIN_LENGTH) first * second
-    else {
-      val (a, b) = split(first)
-      val (c, d) = split(second)
-
-      a * c * T * T + ((a + b) * (c + d) - a * c - b * d) * T + b * d
-    }
-
 
 }
